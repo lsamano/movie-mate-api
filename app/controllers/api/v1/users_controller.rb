9f4e8e3754d10f4e7b27ae35f2b114ff513faeaf
lsamano <leizl.samano@gmail.com>
2019-04-18 15:42:37 -0400

    user show route and action added

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -18,6 +18,11 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+    render json: {user: UserSerializer.new(@user)}
+  end
+
   private
 
   def user_params
