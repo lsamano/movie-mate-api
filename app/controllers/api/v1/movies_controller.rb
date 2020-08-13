ccf8a332908a0caedaf299c0dac395afac67109d
lsamano <leizl.samano@gmail.com>
2020-08-13 13:19:04 -0400

    cleaned up data returned from movies index endpoint

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -3,19 +3,21 @@ class Api::V1::MoviesController < ApplicationController
   before_action :api_key
 
   def index
-    discover = Tmdb::Discover.movie.results
-    upcoming = Tmdb::Movie.upcoming.results
-    now_playing = Tmdb::Movie.now_playing.results
-    popular = Tmdb::Movie.popular.results
-    top_rated = Tmdb::Movie.top_rated.results
+    discover = Tmdb::Discover.movie.results.map { |obj| obj.to_h }
+    upcoming = Tmdb::Movie.upcoming.results.map { |obj| obj.to_h }
+    now_playing = Tmdb::Movie.now_playing.results.map { |obj| obj.to_h }
+    popular = Tmdb::Movie.popular.results.map { |obj| obj.to_h }
+    top_rated = Tmdb::Movie.top_rated.results.map { |obj| obj.to_h }
 
-    render json: {
+    data = {
       discover: discover,
       upcoming: upcoming,
       nowPlaying: now_playing,
       popular: popular,
       topRated: top_rated
     }
+
+    render json: data
   end
 
   def show
@@ -26,7 +28,8 @@ class Api::V1::MoviesController < ApplicationController
   private
 
   def api_key
-    Tmdb::Api.key(Rails.application.credentials.tmdb_key)
+    # Tmdb::Api.key(Rails.application.credentials.tmdb_key)
+    Tmdb::Api.key("9e618d2863d102107605400d64a71060")
   end
 
 end
